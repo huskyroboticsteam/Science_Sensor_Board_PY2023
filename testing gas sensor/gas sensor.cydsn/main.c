4c0f1b8c8e0e82923035d65ea1a6a30cbe8dc1bb
Becca Caulton <53310516+BeccaCaul@users.noreply.github.com>
2025-01-25 12:47:48 -0800

    clarified about pin placement

--- testing gas sensor/gas sensor.cydsn/main.c
+++ testing gas sensor/gas sensor.cydsn/main.c
@@ -8,17 +8,18 @@ int main(void)
 {
     CyGlobalIntEnable; // Enable global interrupts
     
-    UART_Start(); // Start UART
-    ADC_Start();  // Start ADC
+    
+    UART_Start(); // Start UART (make sure uart_rx is P7[0] and uart_tx P7[1] for pins)
+    ADC_Start();  // Start Analog to digital conversion
     ADC_StartConvert(); // Start ADC conversion
-    for(;;)
+    for(;;) //run forever
     {
-         UART_UartPutString(buffer);
+        UART_UartPutString(buffer);
         if(ADC_IsEndConversion(ADC_WAIT_FOR_RESULT)) // Wait for conversion to complete
         {
             int16 value = ADC_GetResult16(0); // Get ADC result from channel 0
             sprintf(buffer, "%d\r\n", value); // Format the value as a string
-            UART_UartPutString(buffer); 
+            UART_UartPutString(buffer); //print the data
         }
         CyDelay(100); // Delay 100 ms
     }
